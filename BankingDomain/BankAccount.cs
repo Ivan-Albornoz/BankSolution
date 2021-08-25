0b3a0e2560be7220b0a0f1e8986aa241e10c9c3a
Ivan-Albornoz <89415907+Ivan-Albornoz@users.noreply.github.com>
2021-08-25 15:04:38 -0400

    Initialize composition

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal _balance = 5000;
+        private readonly ICanCalulateBonuses _bonusCalculator;
+
+        public BankAccount(ICanCalulateBonuses bonusCalculator)
+        {
+            _bonusCalculator = bonusCalculator;
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            if (amountToWithdraw > _balance)
+            {
+                throw new OverdraftException();
+            }
+            else
+            {
+                _balance -= amountToWithdraw;
+            }
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            // WTCYWYH
+            decimal bonus = _bonusCalculator.GetDepositBonusFor(_balance, amountToDeposit);
+            _balance += amountToDeposit + bonus;
+        }
+    }
+}
---
+++ BankingTests/BankAccountTests/NewAccounts.cs
@@ -0,0 +1,25 @@
+﻿using BankingDomain;
+using BankingTests.TestDoubles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace BankingTests
+{
+    public class NewAccounts
+    {
+        [Fact]
+        public void NewAccountsHaveProperBalance()
+        {
+            var account = new BankAccount(new DummyBonusCalculator());
+
+            decimal openingBalance = account.GetBalance();
+
+            Assert.Equal(5000M, openingBalance);
+        }
+    }
+}
